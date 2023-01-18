d8d974c6252f42e88909bd0e2519b8485a044bc7
jjiang9888 <jjiang9888@gmail.com>
2023-01-18 12:41:17 -0500

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Home from './Screen/HomeScreen';
+import Breweries from './Screen/Breweries';
 import BrewCreate from './Screen/BrewCreate';
 import BrewEdit from './Screen/BrewEdit';
 import BrewDet from './Screen/BrewDet';
+import {Routes, Route} from 'react-router-dom';
+import Nav from './Components/Nav';
 import './App.css';
+
 
 function App() {
   return (
- 
-    </div>
+    <div className="BrewApp">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Breweries" element={<Breweries />}/>
+        <Route path="/Breweries/:BrewId" element={<BrewDet/>}/>
+        <Route path="/add-brewery" element={<BrewCreate/>}/>
+        <Route path="/Breweries/:id/edit" element={<BrewEdit/>}/>
+      </Routes>
+</div>
   );
-}
+  }
 
 export default App;
